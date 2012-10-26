18ae164f1ed999c408b8d40ee3588a2ee6608203
Cristian Bica <cristian.bica@gmail.com>
2012-10-26 23:58:35 +0300

    added build script and files

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+rm -rf build
+mkdir build
 xcodebuild -project RHAddressBook.xcodeproj -target RHAddressBook -configuration Release CONFIGURATION_BUILD_DIR=./build && rm -rf build/RHAddressBook.build
+#xcodebuild -project RHAddressBook.xcodeproj -target RHAddressBook -configuration Release CLANG_ENABLE_OBJC_ARC=YES CONFIGURATION_BUILD_DIR=./build && rm -rf build/RHAddressBook.build
