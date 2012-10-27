45287a628b9a1e7e6b58a0956fc7db7fcf03f672
Cristian Bica <cristian.bica@gmail.com>
2012-10-28 00:53:12 +0300

    added ARCHS to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf build build-arc
 mkdir build build-arc
-xcodebuild -project RHAddressBook.xcodeproj -target RHAddressBook -configuration Release CONFIGURATION_BUILD_DIR=./build && rm -rf build/RHAddressBook.build
-xcodebuild -project RHAddressBook.xcodeproj -target RHAddressBook -configuration Release CLANG_ENABLE_OBJC_ARC=YES CONFIGURATION_BUILD_DIR=./build-arc && rm -rf build/RHAddressBook.build
+xcodebuild -project RHAddressBook.xcodeproj -target RHAddressBook -configuration Release ARCHS="armv6 armv7 armv7s i386" CONFIGURATION_BUILD_DIR=./build && rm -rf build/RHAddressBook.build
+xcodebuild -project RHAddressBook.xcodeproj -target RHAddressBook -configuration Release ARCHS="armv6 armv7 armv7s i386" CLANG_ENABLE_OBJC_ARC=YES CONFIGURATION_BUILD_DIR=./build-arc && rm -rf build/RHAddressBook.build
 
